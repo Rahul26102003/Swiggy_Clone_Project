22d0adac46e8a4748be7f80328031d070c927504
Rahul26102003 <2221032cse.coe@cgc.edu.in>
2024-04-13 09:58:10 +0530

    Created a Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Components/Header";
 import Category from "./Components/Category";
 import TopRest from "./Components/TopRest";
 import OnlineDelivery from "./Components/OnlineDelivery";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Category/>
       <TopRest/>
       <OnlineDelivery className="pt-5"/>
+      <Footer/>
     </>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+
+
+
+export default function Footer() {
+    return (
+        <>
+        <div className="pt-[100px] ">
+        <div className="text-[35px] font-extrabold h-[150px] pt-5 bg-gray-200 text-[02060CBF]">
+            <div className="ps-[370px] font-sans">
+                <pre>
+                    For better experience, download <br />
+                    the Swiggy app now  
+                </pre>    
+            </div>
+            <div className="flex gap-[70px] transform translate-y-[-100px]">
+                <a href="https://play.google.com/store/apps/details?id=in.swiggy.android"><img className="h-[110px] ps-[1100px] cursor-pointer" src="images/app_store.png"/></a>
+                <a href="https://play.google.com/store/apps/details?id=in.swiggy.android"><img className="h-[110px] cursor-pointer" src="images/play_store.png"/></a>
+            </div>
+        </div>
+        <footer className="h-[530px] flex w-full">
+            <div className="text-white">
+                <ul>
+                    <li className="flex gap-4 pt-[60px] ps-[300px]">
+                        <svg width="50" height="60" viewBox="0 0 21 32" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_b_674_19294)"><path fill-rule="evenodd" clip-rule="evenodd" d="M20.3819 14.7977C20.5245 13.9563 20.3574 13.3121 19.9261 12.9756C19.2787 12.4711 18.3021 12.1959 15.9859 12.204C14.2724 12.2082 12.4327 12.2167 11.6469 12.2205C11.5739 12.2082 11.3093 12.1181 11.297 11.7938L11.2683 6.64781C11.2683 6.3236 11.5207 6.05694 11.8385 6.05694C12.1559 6.05694 12.412 6.31936 12.412 6.64358C12.412 6.64358 12.4285 9.43839 12.4327 10.4314C12.4327 10.5257 12.4897 10.7513 12.7011 10.8089C14.085 11.1822 21.0778 10.883 20.9639 9.57764C20.3536 4.16496 15.8884 -0.0164585 10.4829 4.87055e-05C8.78164 0.00385807 7.16965 0.426698 5.745 1.16952C2.33815 2.98744 -0.0755529 6.63977 0.00180748 10.8542C0.0546894 13.8374 1.98811 19.1396 3.16037 19.923C3.70173 20.2845 4.40996 20.1491 7.58886 20.1368C9.0298 20.1325 10.3732 20.1325 11.0324 20.1325C11.1016 20.1448 11.4599 20.2312 11.4637 20.5677L11.484 24.4782C11.484 24.8024 11.232 25.0691 10.9142 25.0691C10.5968 25.0691 10.3402 24.8109 10.3402 24.4824C10.3402 24.4824 10.3647 22.9108 10.3609 22.328C10.3609 22.1926 10.3689 21.9585 9.98235 21.7862C8.71622 21.2119 4.60532 21.5729 4.37737 22.2129C4.29191 22.4592 4.75185 23.4073 5.46418 24.6259C7.83331 28.4425 10.1571 31.4176 10.5318 31.8937C10.5563 31.9141 10.5766 31.9348 10.5968 31.9471C10.9104 31.5611 19.1078 22.2747 20.3819 14.7977Z" fill="white"></path></g><defs><filter id="filter0_b_674_19294" x="-9.26002" y="-9.26002" width="39.4853" height="50.4673" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feGaussianBlur in="BackgroundImageFix" stdDeviation="4.63001"></feGaussianBlur><feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_674_19294"></feComposite><feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_674_19294" result="shape"></feBlend></filter></defs></svg>
+                        <h1 className="text-[40px] font-bold">Swiggy</h1>
+                    </li>
+                    <li className="text-[22px] ps-[300px] pt-4 text-slate-gray"><h2>&copy; 2024 Bundl Technologies 
+                        <pre>Pvt. Ltd</pre>
+                        </h2>
+                    </li>
+                </ul>
+            </div>
+            <div className="text-white text-[20px]">
+                <ul>
+                    <li className="py-1 font-bold text-white text-[26px] pt-[65px] ps-[190px]">Company</li>
+                    <li className="py-1 ps-[190px]">About</li>
+                    <li className="py-1 ps-[190px]">Careers</li>
+                    <li className="py-1 ps-[190px]">Team</li>
+                    <li className="py-1 ps-[190px]">Swiggy One</li>
+                    <li className="py-1 ps-[190px]">Swiggy Instamart</li>
+                    <li className="py-1 ps-[190px]">Swiggy Genie</li>
+                </ul>
+            </div>
+            <div  className="text-white text-[20px]">
+                
+                    <ul>
+                        <li className="py-1 font-bold text-white text-[26px] pt-[65px] ps-[190px]">Contact us</li>
+                        <li className="py-1 ps-[190px]">Help & Support</li>
+                        <li className="py-1 ps-[190px]">Partner with us</li>
+                        <li className="py-1 ps-[190px]">Ride with us</li>
+                    </ul>
+                    <ul className="pb-[-160px]">
+                        <li className="py-1 font-bold text-white text-[26px] pt-[65px] ps-[190px]">Legal</li>
+                        <li className="py-1 ps-[190px]">Terms & Conditions</li>
+                        <li className="py-1 ps-[190px]">Cookie Policy</li>
+                        <li className="py-1 ps-[190px]">Privacy Policy</li>
+                    </ul>
+               
+            </div>
+            <div className="text-white text-[20px]">
+                <ul>
+                    <li className="py-1 font-bold text-white text-[26px] pt-[65px] ps-[190px]">We deliver to:</li>
+                    <li className="py-1 ps-[190px]">Bangalore</li>
+                    <li className="py-1 ps-[190px]">Gurgaon</li>
+                    <li className="py-1 ps-[190px]">Delhi</li>
+                    <li className="py-1 ps-[190px]">Mumbai</li>
+                    <li className="py-1 ps-[190px]">Pune</li>
+                    <li className="py-1 ps-[190px]">Chandigarh</li>
+                </ul>
+            </div>
+
+        </footer>
+        </div>
+        </>
+    );
+
+}
